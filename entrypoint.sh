3529e8a935e11361c0b14c6cd162ae93801f1136
Brian Mullin <brian.mullin@karlsgate.com>
2024-09-10 10:45:23 -0400

    Switched to bash shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,45 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
-if [ -z "${INPUT_SOURCE_DIRECTORY}" ]; then
+if [[ -z "${INPUT_SOURCE_DIRECTORY}" ]]; then
   echo "source_directory parameter is required"
   exit 1
 fi
 
-if [ -z "${INPUT_CONTAINER_NAME}" ]; then
+if [[ -z "${INPUT_CONTAINER_NAME}" ]]; then
   echo "container_name parameter is required"
   exit 1
 fi
 
-if [ -z "${INPUT_CONNECTION_STRING}" ]; then
+if [[ -z "${INPUT_CONNECTION_STRING}" ]]; then
   echo "connection_string parameter is required"
   exit 1
 fi
 
-PATTERN_PARAM=${INPUT_PATTERN:""}
-if [ -z "$PATTERN_PARAM" ]; then
-  PATTERN_OPTION=""
-else
+PATTERN_OPTION=""
+PATTERN_PARAM=""
+if [[ -n ${INPUT_PATTERN} ]]; then
   PATTERN_OPTION="--pattern"
+  PATTERN_PARAM=${INPUT_PATTERN}
 fi
 
-CONTENT_TYPE_PARAM=${INPUT_CONTENT_TYPE:""}
-if [ -z "$CONTENT_TYPE_PARAM" ]; then
-  CONTENT_TYPE_OPTION=""
-else
+CONTENT_TYPE_OPTION=""
+CONTENT_TYPE_PARAM=""
+if [[ -n ${INPUT_CONTENT_TYPE} ]]; then
   CONTENT_TYPE_OPTION="--content-type"
+  CONTENT_TYPE_PARAM=${INPUT_CONTENT_TYPE}
 fi
 
-OVERWRITE_PARAM=${INPUT_OVERWRITE:""}
-if [ -z "$OVERWRITE_PARAM" ]; then
-  OVERWRITE_OPTION=""
-else
-  OVERWRITE_OPTION="--overwrite"
+OVERWRITE_OPTION=""
+if [[ -n ${INPUT_OVERWRITE} ]]; then
+  if [[ "${INPUT_OVERWRITE,,}" == "true" ]]; then
+    OVERWRITE_OPTION="--overwrite true"
+  elif [[ "${INPUT_OVERWRITE,,}" == "yes" ]]; then
+    OVERWRITE_OPTION="--overwrite true"
+  fi
 fi
 
 echo "Working from ${PWD}"
 echo "Uploading ${INPUT_SOURCE_DIRECTORY} to ${INPUT_CONTAINER_NAME} ..."
-az storage blob upload-batch --connection-string ${INPUT_CONNECTION_STRING} --source ${INPUT_SOURCE_DIRECTORY} --destination ${INPUT_CONTAINER_NAME} ${PATTERN_OPTION} ${PATTERN_PARAM} ${CONTENT_TYPE_OPTION} ${CONTENT_TYPE_PARAM} ${OVERWRITE_OPTION} ${OVERWRITE_PARAM}
+az storage blob upload-batch --connection-string ${INPUT_CONNECTION_STRING} --source ${INPUT_SOURCE_DIRECTORY} --destination ${INPUT_CONTAINER_NAME} ${PATTERN_OPTION} ${PATTERN_PARAM} ${CONTENT_TYPE_OPTION} ${CONTENT_TYPE_PARAM} ${OVERWRITE_OPTION}
